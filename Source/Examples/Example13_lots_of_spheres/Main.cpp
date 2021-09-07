d2850139f07f46f73104717ecc53dce0c1d716ba
T-rvw <429601557@qq.com>
2021-09-08 00:23:40 +0800

    refactor examples

--- Source/Examples/Example13_lots_of_spheres/Main.cpp
+++ Source/Examples/Example13_lots_of_spheres/Main.cpp
@@ -1,62 +1,62 @@
 #include "Camera.h"
 #include "Dielectric.h"
-#include "HittableList.h"
+#include "ExampleBase.h"
 #include "Lambertian.h"
 #include "MathUtils.h"
 #include "Metal.h"
-#include "ImageExporter.h"
-#include "Ray.h"
 #include "Sphere.h"
 
 #include <iostream>
 #include <vector>
 
-Color getRayColor(const Ray& ray, const HittableList& world, int depth)
+class ExampleLotsOfSpheres : public ExampleBase
 {
-    // If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0)
+public:
+    ExampleLotsOfSpheres(int width, int height) :
+        ExampleBase(width, height)
     {
-        return Color(0.0, 0.0, 0.0);
     }
 
-    std::optional<HitRecord> optHitRecord = world.hit(ray, DOUBLE_EPS, DOUBLE_INFINITY);
-    if (optHitRecord.has_value())
+    Color getRayColor(const Ray& ray, const HittableList& world, int depth)
     {
-        const HitRecord& hitRecord = optHitRecord.value();
-        if (const Hittable* pHitObject = hitRecord.hitObject())
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0)
         {
-            Ray scattered;
-            Color attenuation;
-            if (std::shared_ptr<Material> pMaterial = pHitObject->material())
-            {
-                if (pMaterial->scatter(ray, hitRecord, attenuation, scattered))
-                {
-                    return attenuation * getRayColor(scattered, world, depth - 1);
-                }
+            return Color(0.0, 0.0, 0.0);
+        }
 
-                return Color(0.0, 0.0, 0.0);
+        std::optional<HitRecord> optHitRecord = world.hit(ray, DOUBLE_EPS, DOUBLE_INFINITY);
+        if (optHitRecord.has_value())
+        {
+            const HitRecord& hitRecord = optHitRecord.value();
+            if (const Hittable* pHitObject = hitRecord.hitObject())
+            {
+                Ray scattered;
+                Color attenuation;
+                if (std::shared_ptr<Material> pMaterial = pHitObject->material())
+                {
+                    if (pMaterial->scatter(ray, hitRecord, attenuation, scattered))
+                    {
+                        return attenuation * getRayColor(scattered, world, depth - 1);
+                    }
+
+                    return Color(0.0, 0.0, 0.0);
+                }
             }
         }
-    }
 
-    // background
-	XYZ unitDir = unit(ray.direction());
-	double factor = 0.5 * (unitDir.y() + 1.0);
-	return (1.0 - factor) * Color(1.0, 1.0, 1.0) + factor * Color(0.5, 0.7, 1.0);
-}
+        // background
+        XYZ unitDir = unit(ray.direction());
+        double factor = 0.5 * (unitDir.y() + 1.0);
+        return (1.0 - factor) * Color(1.0, 1.0, 1.0) + factor * Color(0.5, 0.7, 1.0);
+    }
+};
 
 int main()
 {
     // ImageExporter
     constexpr int imageWidth = 1200;
     constexpr int imageHeight = 800;
-    constexpr int pixelNumber = imageHeight * imageWidth;
-    constexpr double aspectRatio = static_cast<double>(imageWidth) / static_cast<double>(imageHeight);
-    ImageExporter imageExporter(imageWidth, imageHeight);
-
-    // Config
-    constexpr int samplesPerPixel = 500;
-    constexpr int maxRecursiveDepth = 50;
 
     // World
     auto groupMaterial = std::make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
@@ -105,49 +105,15 @@ int main()
     double distToFocus = 10.0;
     double aperture = 0.1;
 
+    constexpr double aspectRatio = static_cast<double>(imageWidth) / static_cast<double>(imageHeight);
     Camera camera(lookFrom, lookAt, vup, aperture, distToFocus, 20.0, aspectRatio);
 
-	// Render
-    int curPixelCount = 0;
-#pragma omp parallel for
-    for (int jj = imageHeight - 1; jj >= 0; --jj)
-    {
-        for (int ii = 0; ii < imageWidth; ++ii)
-        {
-            Color pixelColor(0.0, 0.0, 0.0);
-
-            for (int sampleTimes = 0; sampleTimes < samplesPerPixel; ++sampleTimes)
-            {
-                double u = static_cast<double>(ii + randomDouble()) / (imageWidth - 1);
-                double v = static_cast<double>(jj + randomDouble()) / (imageHeight - 1);
-                Ray ray = camera.getRay(u, v);
-                pixelColor += getRayColor(ray, hittableList, maxRecursiveDepth);
-            }
-
-            // sample && gamma-correct(1/2).
-            {
-                double sampleScale = 1.0 / samplesPerPixel;
-                pixelColor[0] = clamp(pow(pixelColor.x() * sampleScale, 0.5), 0.0, 1.0);
-                pixelColor[1] = clamp(pow(pixelColor.y() * sampleScale, 0.5), 0.0, 1.0);
-                pixelColor[2] = clamp(pow(pixelColor.z() * sampleScale, 0.5), 0.0, 1.0);
-            }
-
-            // (imageHeight - 1 - jj) * imageWidth + ii;
-            size_t pixelIndex = pixelNumber - (jj + 1) * imageWidth + ii;
-            imageExporter.fillColor(pixelIndex, pixelColor);
-
-            std::cout << std::format("Fill color pixel placed at {}, progress = {}/{}\n", pixelIndex, ++curPixelCount, pixelNumber);
-        }
-    }
-
-    if (imageExporter.generate("test.png"))
-    {
-        std::cout << "Succeed to generate image." << std::endl;
-    }
-    else
-    {
-        std::cout << "Failed to generate image." << std::endl;
-    }
+    // Example init
+    ExampleLotsOfSpheres example(imageWidth, imageHeight);
+    example.setSampleTimes(500);
+    example.setMaxRecursiveDepth(50);
+    example.process(camera, hittableList);
+    example.generate("test.png");
 
     return 0;
 }
